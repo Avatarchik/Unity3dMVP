15777b37dbdb462151f62955522854c8d8ac3228
经聪 <jingcong1984@hotmail.com>
2017-04-11 00:42:22 +0800

    Add base view

--- Assets/Haruna/MVP/View/BaseView.cs
+++ Assets/Haruna/MVP/View/BaseView.cs
@@ -2,17 +2,68 @@
 using System.Collections.Generic;
 using System.Reflection;
 using UnityEngine;
+using UnityEngine.Events;
 
-namespace Haruna.UnityMVP.Presenter
+namespace Haruna.Unity.View
 {
 	public class BaseView : MonoBehaviour
 	{
 		[SerializeField]
-		bool _allowMultiple;
+		UnityEvent _initializeEvent;
+		[SerializeField]
+		UnityEvent _showEvent;
+		[SerializeField]
+		UnityEvent _hideEvent;
 
-		public void Initialize()
+		//[SerializeField]
+		//PanelLayerEnum _sortingLayer = PanelLayerEnum.Default;
+		//public PanelLayerEnum SortingLayer { get { return _sortingLayer; } }
+		[SerializeField]
+		bool _setActiveOnShow;
+		[SerializeField]
+		bool _setDeactiveOnHide;
+
+		public enum ActionOnInitializeEnum
 		{
-
+			DoNothing,
+			ShowOnInitializing,
+			HideOnInitializing
 		}
+		[SerializeField]
+		ActionOnInitializeEnum _actionOnInializing = ActionOnInitializeEnum.DoNothing;
+		
+		public virtual void Init()
+		{
+			if (_initializeEvent != null)
+				_initializeEvent.Invoke();
+
+			if (_actionOnInializing == ActionOnInitializeEnum.ShowOnInitializing)
+				Show();
+			else if (_actionOnInializing == ActionOnInitializeEnum.HideOnInitializing)
+				Hide();
+		}
+
+		protected virtual void Start()
+		{
+		}
+
+		public virtual void Show()
+		{
+			if (_setActiveOnShow)
+				gameObject.SetActive(true);
+
+			if (_showEvent != null)
+				_showEvent.Invoke();
+		}
+
+		public virtual void Hide()
+		{
+			if (_setDeactiveOnHide)
+				gameObject.SetActive(false);
+
+			if (_hideEvent != null)
+				_hideEvent.Invoke();
+		}
+
 	}
 }
