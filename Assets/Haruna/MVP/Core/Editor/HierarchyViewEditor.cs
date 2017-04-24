059a6bee6932686708f2f6f4f87fc73db6dd3eed
经聪 <jingcong1984@hotmail.com>
2017-04-25 00:07:16 +0800

    Add icon display to hierarchy. Add preference to set icon display and space to hierarchy end.

---
+++ Assets/Haruna/MVP/Core/Editor/HierarchyViewEditor.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+namespace Haruna.UnityMVP.Presenter
+{
+	[InitializeOnLoad]
+	public class HierarchyViewEditor
+	{
+		static HierarchyViewEditor()
+		{
+			EditorApplication.hierarchyWindowItemOnGUI += OnHierarchyGUI;
+		}
+		
+		static float _iconWidth = 10f;
+		static float _iconHeight = 9f;
+		static float _space = 2f;
+
+		static void OnHierarchyGUI(int instanceID, Rect selectionRect)
+		{
+			var wdithToRight = EditorPrefs.GetFloat(PreferenceEditor.SpaceToHerarchyEnd, 0);
+
+			var obj = EditorUtility.InstanceIDToObject(instanceID) as GameObject;
+			if (obj == null)
+				return;
+
+			List<DrawIcon> toDrawList = new List<DrawIcon>();
+
+			var actionLinker = obj.GetComponent<PresenterActionLinker>();
+			var eventLinker = obj.GetComponent<PresenterEventLinker>();
+			if(EditorPrefs.GetBool(PreferenceEditor.ShowLinkerIconInHierarchy, true)
+				&& (actionLinker != null || eventLinker != null))
+			{
+				toDrawList.Add(new DrawIcon() { IconColor = Color.red });
+			}
+
+			{
+				var binder = obj.GetComponent<Model.IMvpObjectBinder>();
+				if (EditorPrefs.GetBool(PreferenceEditor.ShowObjectBinderIconInHierarchy, true) && binder != null)
+					toDrawList.Add(new DrawIcon() { IconColor = Color.yellow });
+			}
+
+			{
+				var binder = obj.GetComponent<Model.IMvpArrayBinder>();
+				if (EditorPrefs.GetBool(PreferenceEditor.ShowArrayBinderIconInHierarchy, true) && binder != null)
+					toDrawList.Add(new DrawIcon() { IconColor = Color.yellow });
+			}
+			{
+				var binder = obj.GetComponent<Model.IMvpBoolBinder>();
+				if (EditorPrefs.GetBool(PreferenceEditor.ShowBoolBinderIconInHierarchy, false) && binder != null)
+					toDrawList.Add(new DrawIcon() { IconColor = Color.yellow });
+			}
+			{
+				var binder = obj.GetComponent<Model.IMvpFloatBinder>();
+				if (EditorPrefs.GetBool(PreferenceEditor.ShowFloatBinderIconInHierarchy, false) && binder != null)
+					toDrawList.Add(new DrawIcon() { IconColor = Color.yellow });
+			}
+			{
+				var binder = obj.GetComponent<Model.IMvpStringBinder>();
+				if (EditorPrefs.GetBool(PreferenceEditor.ShowStringBinderIconInHierarchy, false) && binder != null)
+					toDrawList.Add(new DrawIcon() { IconColor = Color.yellow });
+			}
+			{
+				var binders = obj.GetComponents<Component>();
+				if (EditorPrefs.GetBool(PreferenceEditor.ShowCustomBinderIconInHierarchy, false) 
+					&& binders.Any(b => b.GetType().GetInterface(typeof(Model.IMvpCustomTypeBinder<>).FullName) != null))
+					toDrawList.Add(new DrawIcon() { IconColor = Color.yellow });
+			}
+
+			var rect = selectionRect;
+			rect.x = rect.x + rect.width - wdithToRight;
+			rect.width = _iconWidth;
+			rect.y += (rect.height - _iconHeight) / 2f;
+			rect.height = _iconHeight;
+
+			foreach(var toDraw in toDrawList)
+			{
+				rect.x -= _iconWidth;
+				EditorGUI.DrawRect(rect, toDraw.IconColor);
+				rect.x -= _space;
+			}
+		}
+		
+		struct DrawIcon
+		{
+			public Color IconColor;
+		}
+	}
+}
---
+++ Assets/Haruna/MVP/Core/Editor/PreferenceEditor.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+namespace Haruna.UnityMVP.Presenter
+{
+	[InitializeOnLoad]
+	public class PreferenceEditor
+	{
+		public const string SpaceToHerarchyEnd = "Haruna_UnityMVP_SpaceToHerarchyEnd";
+		public const string ShowLinkerIconInHierarchy = "Haruna_UnityMVP_ShowLinkerIconInHierarchy";
+		public const string ShowObjectBinderIconInHierarchy = "Haruna_UnityMVP_ShowModelBinderIconInHierarchy";
+		public const string ShowArrayBinderIconInHierarchy = "Haruna_UnityMVP_ShowArrayBinderIconInHierarchy";
+		public const string ShowFloatBinderIconInHierarchy = "Haruna_UnityMVP_ShowFloatBinderIconInHierarchy";
+		public const string ShowStringBinderIconInHierarchy = "Haruna_UnityMVP_ShowStringBinderIconInHierarchy";
+		public const string ShowBoolBinderIconInHierarchy = "Haruna_UnityMVP_ShowBoolBinderIconInHierarchy";
+		public const string ShowCustomBinderIconInHierarchy = "Haruna_UnityMVP_ShowCustomBinderIconInHierarchy";
+
+		static Vector2 _scrollPos;
+		[PreferenceItem("Unity3d MVP")]
+		static void OnPrefsGUI()
+		{
+			_scrollPos = EditorGUILayout.BeginScrollView(_scrollPos);
+			{
+				var value = EditorPrefs.GetFloat(SpaceToHerarchyEnd);
+				value = EditorGUILayout.FloatField("Space To Hierachy End", value);
+				EditorPrefs.SetFloat(SpaceToHerarchyEnd, value);
+			}
+			{
+				var value = EditorPrefs.GetBool(ShowLinkerIconInHierarchy, true);
+				value = EditorGUILayout.Toggle("Show Linker Icon", value);
+				EditorPrefs.SetBool(ShowLinkerIconInHierarchy, value);
+			}
+			{
+				var value = EditorPrefs.GetBool(ShowObjectBinderIconInHierarchy, true);
+				value = EditorGUILayout.Toggle("Show Object Binder Icon", value);
+				EditorPrefs.SetBool(ShowObjectBinderIconInHierarchy, value);
+			}
+			{
+				var value = EditorPrefs.GetBool(ShowArrayBinderIconInHierarchy, true);
+				value = EditorGUILayout.Toggle("Show Array Binder Icon", value);
+				EditorPrefs.SetBool(ShowArrayBinderIconInHierarchy, value);
+			}
+			{
+				var value = EditorPrefs.GetBool(ShowBoolBinderIconInHierarchy, false);
+				value = EditorGUILayout.Toggle("Show Bool Binder Icon", value);
+				EditorPrefs.SetBool(ShowBoolBinderIconInHierarchy, value);
+			}
+			{
+				var value = EditorPrefs.GetBool(ShowStringBinderIconInHierarchy, false);
+				value = EditorGUILayout.Toggle("Show String Binder Icon", value);
+				EditorPrefs.SetBool(ShowStringBinderIconInHierarchy, value);
+			}
+			{
+				var value = EditorPrefs.GetBool(ShowFloatBinderIconInHierarchy, false);
+				value = EditorGUILayout.Toggle("Show Float Binder Icon", value);
+				EditorPrefs.SetBool(ShowFloatBinderIconInHierarchy, value);
+			}
+			{
+				var value = EditorPrefs.GetBool(ShowCustomBinderIconInHierarchy, false);
+				value = EditorGUILayout.Toggle("Show Custom Binder Icon", value);
+				EditorPrefs.SetBool(ShowCustomBinderIconInHierarchy, value);
+			}
+			EditorGUILayout.EndScrollView();
+		}
+	}
+}
